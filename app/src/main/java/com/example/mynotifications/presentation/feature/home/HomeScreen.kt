75f5fb7363d6f8a9c84a72ea3188f0d80f5612ac
Guilherme Magro <guilherme.magro@hotmail.com>
2023-12-15 23:32:03 -0300

    Adds button to go to device Notifications Listener settings

--- app/src/main/java/com/example/mynotifications/presentation/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/mynotifications/presentation/feature/home/HomeScreen.kt
@@ -1,19 +1,24 @@
 package com.example.mynotifications.presentation.feature.home
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mynotifications.presentation.component.NotificationItemView
 import com.example.mynotifications.presentation.theme.MyNotificationsTheme
@@ -24,6 +29,8 @@ fun HomeScreen(
     viewModel: HomeViewModel = viewModel()
 ) {
     val notifications by viewModel.notifications.collectAsState(initial = emptyList())
+    val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+    val context = LocalContext.current
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -34,6 +41,13 @@ fun HomeScreen(
             contentPadding = PaddingValues(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
+            item {
+                Button(
+                    onClick = { startActivity(context, intent, null) }
+                ) {
+                    Text(text = "Open Notifications Listener settings")
+                }
+            }
             items(notifications) { notification ->
                 with(notification) {
                     NotificationItemView(
