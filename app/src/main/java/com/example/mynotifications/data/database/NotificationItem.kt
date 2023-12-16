dd7f222276674b8bc107124e5503b14b5e018009
Guilherme Magro <guilherme.magro@hotmail.com>
2023-12-15 22:38:44 -0300

    Adds notification creation logic and adds NotificationItem fields and equals logic

--- app/src/main/java/com/example/mynotifications/data/database/NotificationItem.kt
+++ app/src/main/java/com/example/mynotifications/data/database/NotificationItem.kt
@@ -2,12 +2,41 @@ package com.example.mynotifications.data.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlin.math.abs
+import kotlin.time.Duration.Companion.seconds
+
+private val oneSecond = 1.seconds.inWholeMilliseconds
 
 @Entity
 data class NotificationItem(
     @PrimaryKey(autoGenerate = true) val uid: Int,
     val title: String,
     val message: String,
+    val packageName: String,
+    val postTime: Long,
 ) {
-    constructor(title: String, message: String) : this(0, title, message)
+    constructor(
+        title: String,
+        message: String,
+        packageName: String,
+        postTime: Long
+    ) : this(0, title, message, packageName, postTime)
+
+    override fun equals(other: Any?): Boolean {
+        return ((other != null)
+                && (other is NotificationItem)
+                && (other.title == title)
+                && (other.message == message)
+                && (other.packageName == packageName))
+                && (abs(other.postTime - postTime) < oneSecond)
+    }
+
+    override fun hashCode(): Int {
+        var result = uid
+        result = 31 * result + title.hashCode()
+        result = 31 * result + message.hashCode()
+        result = 31 * result + packageName.hashCode()
+        result = 31 * result + postTime.hashCode()
+        return result
+    }
 }
--- app/src/main/java/com/example/mynotifications/data/service/AppNotificationListenerService.kt
+++ app/src/main/java/com/example/mynotifications/data/service/AppNotificationListenerService.kt
@@ -1,5 +1,6 @@
 package com.example.mynotifications.data.service
 
+import android.os.Bundle
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
@@ -12,11 +13,16 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+private const val TITLE_KEY = "android.title"
+private const val TEXT_KEY = "android.text"
+
 @AndroidEntryPoint
 class AppNotificationListenerService : NotificationListenerService() {
 
     private val job = SupervisorJob()
     private val scope = CoroutineScope(Dispatchers.IO + job)
+
+    private var lastNotification: NotificationItem? = null
 
     @Inject
     lateinit var notificationRepository: NotificationRepository
@@ -28,22 +34,46 @@ class AppNotificationListenerService : NotificationListenerService() {
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
-        // TODO - Ignore if has the same contents of the last notification
+
+        val sbnExtrasBundle = sbn?.notification?.extras
         val notification = sbn?.notification
         val packageName = sbn?.packageName
-//        sbn?.postTime // horario timestamp - 1702158893946
-//        sbn?.notification?.tickerText // titulo
+        val postTime = sbn?.postTime
+        sbnExtrasBundle?.let { tryToInsertNotification(it, packageName, postTime) }
 
-        scope.launch {
-            notificationRepository.insert(NotificationItem(
-                title = packageName ?: "Title",
-                message = "Message"
-            ))
-        }
+        Log.d("MyNotifications", "extras = " + bundleToString(sbnExtrasBundle))
         Log.d(
             "MyNotifications",
             "onNotificationPosted: Notification = $notification | PackageName = $packageName"
         )
+    }
+
+    private fun tryToInsertNotification(
+        extrasBundle: Bundle?,
+        packageName: String?,
+        postTime: Long?
+    ) {
+        val title = extrasBundle?.getString(TITLE_KEY)
+        val text = extrasBundle?.getString(TEXT_KEY)
+
+        if (!title.isNullOrBlank()
+            && !text.isNullOrBlank()
+            && !packageName.isNullOrBlank()
+            && postTime != null
+        ) {
+            val notificationItem = NotificationItem(
+                title = title,
+                message = text,
+                packageName = packageName,
+                postTime = postTime
+            )
+            if (notificationItem != lastNotification) {
+                scope.launch {
+                    notificationRepository.insert(notificationItem)
+                }
+                lastNotification = notificationItem
+            }
+        }
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification?) {
@@ -57,4 +87,42 @@ class AppNotificationListenerService : NotificationListenerService() {
             "onNotificationRemoved: Notification = $notification | PackageName = $packageName"
         )
     }
+
+    fun bundleToString(bundle: Bundle?): String? {
+        if (bundle == null) {
+            return null
+        }
+        var string = "Bundle{"
+        for (key in bundle.keySet()) {
+            string += " " + key + " => " + bundle[key] + ";"
+        }
+        string += " }Bundle"
+        return string
+    }
 }
+
+//extras = Bundle{
+//android.title => (650) 555-1212;
+//android.hiddenConversationTitle => null;
+//android.reduced.images => true;
+//android.subText => null;
+//android.template => android.app.Notification$MessagingStyle;
+//android.showChronometer => false;
+//android.text => Android is always a sweet treat!;
+//android.progress => 0;
+//android.progressMax => 0;
+//android.selfDisplayName => You;
+//android.conversationUnreadMessageCount => 0;
+//android.appInfo => ApplicationInfo{42ab3ef com.google.android.apps.messaging};
+//android.messages => [Landroid.os.Parcelable;@2db9efc;
+//android.showWhen => true;
+//android.largeIcon => null;
+//android.messagingStyleUser => Bundle[mParcelledData.dataSize=468];
+//android.messagingUser => android.app.Person@7ddb8047;
+//android.infoText => null;
+//android.wearable.EXTENSIONS => Bundle[mParcelledData.dataSize=716];
+//android.progressIndeterminate => false;
+//android.remoteInputHistory => null;
+//android.support.v4.app.extra.COMPAT_TEMPLATE => androidx.core.app.NotificationCompat$MessagingStyle;
+//android.isGroupConversation => false;
+//}Bundle
