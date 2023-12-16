d7759347e1ab3e88465c051c9ecc4c7b481b99df
Guilherme Magro <guilherme.magro@hotmail.com>
2023-12-16 20:26:16 -0300

    Adds try catch to getBitmapFromOrNull extension

--- app/src/main/java/com/example/mynotifications/presentation/extensions/ContextExt.kt
+++ app/src/main/java/com/example/mynotifications/presentation/extensions/ContextExt.kt
@@ -2,13 +2,18 @@ package com.example.mynotifications.presentation.extensions
 
 import android.content.Context
 import android.content.Context.CONTEXT_IGNORE_SECURITY
+import android.content.pm.PackageManager.NameNotFoundException
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.core.graphics.drawable.toBitmapOrNull
 
 fun Context.getBitmapFromOrNull(id: Int, packageName: String): ImageBitmap? {
-    return createPackageContext(packageName, CONTEXT_IGNORE_SECURITY)
-        ?.getDrawable(id)
-        ?.toBitmapOrNull()
-        ?.asImageBitmap()
+    return try {
+        createPackageContext(packageName, CONTEXT_IGNORE_SECURITY)
+            ?.getDrawable(id)
+            ?.toBitmapOrNull()
+            ?.asImageBitmap()
+    } catch (e: NameNotFoundException) {
+        null
+    }
 }
