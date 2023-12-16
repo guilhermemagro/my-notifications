93c36320ca69ed83282345e79a9de03c75fd90f3
Guilherme Magro <guilherme.magro@hotmail.com>
2023-12-16 20:17:22 -0300

    NotificationItemView layout improvement

--- app/src/main/java/com/example/mynotifications/presentation/component/NotificationItemView.kt
+++ app/src/main/java/com/example/mynotifications/presentation/component/NotificationItemView.kt
@@ -1,11 +1,14 @@
 package com.example.mynotifications.presentation.component
 
 import android.content.Context
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mynotifications.presentation.extensions.getBitmapFromOrNull
 
 @Composable
@@ -18,14 +21,16 @@ fun NotificationItemView(
     iconRes: Int? = null,
     context: Context? = null,
 ) {
-    Card(modifier = modifier) {
-        iconRes?.let { res ->
-            val iconBitmap = context?.getBitmapFromOrNull(res, appName)
-            iconBitmap?.let { bitmap ->
-                Icon(bitmap = bitmap, contentDescription = null)
+    Card(modifier = modifier.padding(8.dp)) {
+        Row {
+            iconRes?.let { res ->
+                val iconBitmap = context?.getBitmapFromOrNull(res, appName)
+                iconBitmap?.let { bitmap ->
+                    Icon(bitmap = bitmap, contentDescription = null)
+                }
             }
+            Text(text = appName)
         }
-        Text(text = appName)
         Text(text = postTime)
         Text(text = title)
         Text(text = message)
