fe9ea7fab8ac4103b91c36daa7d4764bb345cbdb
Guilherme Magro <guilherme.magro@hotmail.com>
2023-12-19 22:02:39 -0300

    UI improvements on DismissibleNotificationItemView

--- app/src/main/java/com/example/mynotifications/presentation/component/DismissibleNotificationItemView.kt
+++ app/src/main/java/com/example/mynotifications/presentation/component/DismissibleNotificationItemView.kt
@@ -3,21 +3,27 @@ package com.example.mynotifications.presentation.component
 import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mynotifications.presentation.theme.MyNotificationsTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,17 +50,7 @@ fun DismissibleNotificationItemView(
         modifier = modifier,
         state = dismissState,
         directions = setOf(DismissDirection.EndToStart),
-        background = {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Red)
-                    .padding(horizontal = 8.dp),
-                contentAlignment = Alignment.CenterEnd,
-            ) {
-                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
-            }
-        },
+        background = { SwipeBackground() },
         dismissContent = {
             NotificationItemView(
                 modifier = Modifier.fillMaxSize(),
@@ -67,4 +63,37 @@ fun DismissibleNotificationItemView(
             )
         }
     )
+}
+
+@Composable
+fun RowScope.SwipeBackground(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(AbsoluteRoundedCornerShape(12.0.dp))
+            .background(Color.Red)
+            .padding(horizontal = 8.dp),
+        contentAlignment = Alignment.CenterEnd,
+    ) {
+        Icon(
+            imageVector = Icons.Default.Delete,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.background
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DismissibleNotificationItemViewPreview() {
+    MyNotificationsTheme {
+        DismissibleNotificationItemView(
+            title = "Titulo teste",
+            message = "Mensagem teste teste brown fox",
+            appName = "Instagram",
+            postTime = "10/10/2024 - 15:30:20"
+        )
+    }
 }
--- app/src/main/java/com/example/mynotifications/presentation/component/NotificationItemView.kt
+++ app/src/main/java/com/example/mynotifications/presentation/component/NotificationItemView.kt
@@ -1,17 +1,28 @@
 package com.example.mynotifications.presentation.component
 
 import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight.Companion.W500
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mynotifications.presentation.extensions.getBitmapFromOrNull
+import com.example.mynotifications.presentation.theme.MyNotificationsTheme
 
 @Composable
 fun NotificationItemView(
@@ -23,22 +34,48 @@ fun NotificationItemView(
     iconRes: Int? = null,
     context: Context? = null,
 ) {
-    Card(modifier = modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(12.dp),
+            )
+            .border(1.dp, MaterialTheme.colorScheme.outline, RoundedCornerShape(12.dp))
+            .padding(8.dp)
+    ) {
         Row {
             iconRes?.let { res ->
                 val iconBitmap = context?.getBitmapFromOrNull(res, appName)
                 iconBitmap?.let { bitmap ->
                     Icon(
-                        modifier = Modifier.size(20.dp),
+                        modifier = Modifier
+                            .size(20.dp)
+                            .padding(end = 4.dp),
                         bitmap = bitmap,
                         contentDescription = null
                     )
                 }
             }
-            Text(text = appName)
+            Text(text = appName, style = MaterialTheme.typography.bodySmall)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = postTime, style = MaterialTheme.typography.bodySmall)
         }
-        Text(text = postTime)
-        Text(text = title)
-        Text(text = message)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = title, fontWeight = W500, style = MaterialTheme.typography.bodySmall)
+        Text(text = message, style = MaterialTheme.typography.bodySmall)
+    }
+}
+
+@Preview
+@Composable
+fun NotificationItemViewPreview() {
+    MyNotificationsTheme {
+        NotificationItemView(
+            title = "Titulo teste",
+            message = "Mensagem teste teste brown fox",
+            appName = "Instagram",
+            postTime = "10/10/2024 - 15:30:20",
+        )
     }
 }
