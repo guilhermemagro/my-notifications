f2fa89cdf2a14d596377c1b8a02bfd63f1133593
Guilherme Resende Magro <guilherme.magro@hotmail.com>
2023-12-29 19:16:03 -0300

    Changes format of toFormattedDate

--- app/src/main/java/com/example/mynotifications/data/extensions/DateExt.kt
+++ app/src/main/java/com/example/mynotifications/data/extensions/DateExt.kt
@@ -4,12 +4,12 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-private const val DATE_FORMAT = "dd/MM/yyyy"
+private const val DATE_FORMAT = "EEEE, dd MMM yyyy"
 private const val HOUR_FORMAT = "HH:mm:ss"
 private val defaultLocale = Locale("pt", "BR")
 
 /**
- * Returns a String with date in format dd/MM/yyyy.
+ * Returns a String with date in format EEEE, dd MMM yyyy.
  */
 fun Long.toFormattedDate(): String {
     val date = Date(this)
